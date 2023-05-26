79d3b6dbf7b79a6b136d088342b076291c69acfa
Chandru-Ghub <chandruinfo455@gmail.com>
2023-05-26 14:49:48 +0530

    first commit

--- countriefromASIANcontinent.js
+++ countriefromASIANcontinent.js
@@ -1,4 +1,4 @@
-
+////Countries of ASIAN CONTINENT........
 function getCountry(){
     let countries = JSON.parse(this.responseText);
     
@@ -6,7 +6,24 @@ function getCountry(){
          console.log(k);   
 }
 
-const request = new XMLHttpRequest();
+var request = new XMLHttpRequest();
+request.addEventListener('load',getCountry);
+request.open('GET','https://restcountries.com/v3.1/all')
+request.send();
+
+
+
+
+
+////Countries of ASIAN REGION........
+function getCountry(){
+    let countries = JSON.parse(this.responseText);
+    
+         k = countries.filter((a)=> a.region === 'Asia');
+         console.log(k);   
+}
+
+var request = new XMLHttpRequest();
 request.addEventListener('load',getCountry);
 request.open('GET','https://restcountries.com/v3.1/all')
 request.send();
