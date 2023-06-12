eb3fa27d536a61a56043be2d5e7722428541e4fe
Chandru-Ghub <chandruinfo455@gmail.com>
2023-06-12 22:35:03 +0530

    region to continents

--- countriefromASIANcontinent.js
+++ countriefromASIANcontinent.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 ////Countries of ASIAN CONTINENT........
 function getCountry(){
     let countries = JSON.parse(this.responseText);
     
-         k = countries.filter((a)=> a.continent === 'Asia');
+         k = countries.filter((a)=> a.continents[0] === 'Asia');
          console.log(k);   
 }
 
@@ -18,7 +17,7 @@ request.send();
 
 
 ////Countries of ASIAN REGION........
-function getCountry(){
+ function getCountry(){
     let countries = JSON.parse(this.responseText);
     
          k = countries.filter((a)=> a.region === 'Asia');
@@ -28,39 +27,4 @@ function getCountry(){
 var request = new XMLHttpRequest();
 request.addEventListener('load',getCountry);
 request.open('GET','https://restcountries.com/v3.1/all')
-request.send();
-
-=======
-////Countries of ASIAN CONTINENT........
-function getCountry(){
-    let countries = JSON.parse(this.responseText);
-    
-         k = countries.filter((a)=> a.region === 'Asia');
-         console.log(k);   
-}
-
-var request = new XMLHttpRequest();
-request.addEventListener('load',getCountry);
-request.open('GET','https://restcountries.com/v3.1/all')
-request.send();
-
-
-
-
-
-////Countries of ASIAN REGION........
-function getCountry(){
-    let countries = JSON.parse(this.responseText);
-    
-         k = countries.filter((a)=> a.region === 'Asia');
-         console.log(k);   
-}
-
-var request = new XMLHttpRequest();
-request.addEventListener('load',getCountry);
-request.open('GET','https://restcountries.com/v3.1/all')
-request.send();
-
->>>>>>> bb99a60f5e73e48d68860c5d373c002e56907f39
-
-
+request.send(); 
